7f1192cc0febc629e213b6df84d536dee72e9731
monseleon21 <121038307@upq.edu.mx>
2025-02-24 18:39:32 -0600

    creacion de base de datos

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template
 
+import mysql.connector
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    conn = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='monchisleon21',
+    database='db'
+    )
+    cursor = conn.cursor()
+    cursor.execute("select * from students")
+    students = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return render_template('index.html', students=students)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ database_students.sql
@@ -0,0 +1,15 @@
+create database db;
+
+use db;
+
+CREATE TABLE students(
+    StudentID int not null AUTO_INCREMENT,
+    FirstName varchar(100) NOT NULL,
+    Surname varchar(100) NOT NULL,
+    PRIMARY KEY (StudentID)
+);
+
+INSERT INTO students(FirstName, Surname)
+VALUES("John", "Andersen"), ("Emma", "Smith");
+
+SELECT * FROM students;
